3b28dccee08ccec055d4cf3f7a03217cd6fb9c3d
admin <admin>
2017-09-27 17:36:45 +0800

    sadf

---
+++ app/main.js
@@ -0,0 +1,6 @@
+const greeter = require('./Greeter.js');
+import React from "react";
+import ReactDOM from 'react-dom';
+import Greeter from "./Greeter";
+import "./main.css" 
+ReactDOM.render(<Greeter />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./public",
+    historyApiFallback: true,
+    inline: true
+  },
+  module: {
+    rules: [{
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "es2015", "react"
+            ]
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+
+        use: [{
+            loader: "style-loader"
+          }, {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: "postcss-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin('版权所有，翻版必究')
+  ],
+}
